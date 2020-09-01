725fa39a8c0e9db9106960cfaa036528e29f374b
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2020-09-01 16:31:47 +0300

    практика

---
+++ task3.js
@@ -0,0 +1,16 @@
+// c = 15;
+// d = 2;
+// result = c + d;
+// console.log(result);
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// console.log(a + b + c);
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
---
+++ task4.js
@@ -0,0 +1,52 @@
+// let str = "Привет, Мир!";
+// console.log(str);
+
+// let str1 = "Привет, ";
+// let str2 = "Мир!";
+// console.log(str1 + str2);
+
+// let name = "Надя";
+// console.log("Привет, " + name + "!");
+
+// let number = 2;
+// console.log(number * 2);
+
+// let str = "abcde";
+
+// console.log(str[0], str[2], str[4]);
+
+// let num = "12345";
+// console.log(num[0] * num[1] * num[2] * num[3] * num[4]);
+
+// let minute = 60;
+// let ourSec = minute * 60;
+// let sutki = ourSec * 24;
+// let month = sutki * 30;
+// console.log(ourSec, sutki, month);
+
+// let our = "16:";
+// let minute = "09:";
+// let second = "30";
+
+// console.log(our + minute + second);
+
+// let arr = [2, 5, 3, 9];
+// let result = arr[0] * arr[1] + arr[2] * arr[3];
+// console.log(result);
+
+// let obj = { Коля: "1000", Вася: "500", Петя: "200" };
+// console.log(obj["Петя"], obj["Коля"]);
+
+// let obj = { пн: 1, вт: 2, ср: 3, чт: 4, пт: 5, сб: 6, вс: 7 };
+// console.log(obj["вт"]);
+
+let styles = ["Джаз", "Блюз"];
+console.log(styles);
+styles.push("Рок - н - ролл");
+console.log(styles);
+styles[1] = "Классика";
+console.log(styles);
+styles.shift(0);
+console.log(styles);
+styles.unshift("Рэп", "Регги");
+console.log(styles);
